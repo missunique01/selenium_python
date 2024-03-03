f1fae27587fb7c4883b1c82bb961c7260a44de9b
Abdul <abdulnazma999@gmail.com>
2024-03-03 20:09:50 +0530

    Day19 Selenium Practice

--- Day19_Selenium/webTable_Dynamic.py
+++ Day19_Selenium/webTable_Dynamic.py
@@ -22,18 +22,30 @@ driver_obj.find_element(By.XPATH,"//input[@name='username']").send_keys("Admin")
 driver_obj.find_element(By.XPATH,"//input[@name='password']").send_keys("admin123")
 driver_obj.find_element(By.XPATH,"//button[@type='submit']").click()
 
-#Go to Admin
+# Admin --> USer Management ---> Users
 driver_obj.find_element(By.LINK_TEXT,"Admin").click()
-#Go to user Management
 driver_obj.find_element(By.XPATH,"//li[1]/span[@class='oxd-topbar-body-nav-tab-item']").click()
-#Go to USers
 driver_obj.find_element(By.XPATH,"//a[@class='oxd-topbar-body-nav-tab-link']").click()
 
-noOfRows = driver_obj.find_elements(By.XPATH,"//div[@class='oxd-table']//div[@class='oxd-table-body oxd-card-table-body']")
-print(noOfRows)
-# for r in range(1,len(noOfRows)+1):
-#     print(r)
 
-# //div[@class='orangehrm-container']
-# //div[@class='orangehrm-container']/div[@class='oxd-table']/div[@class='rowgroup']
-# //*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]
+#Check How many users are enabled and how many are in disabled status
+
+noOfRows = driver_obj.find_elements(By.XPATH,"//div[@class='oxd-table-body']/div[@class='oxd-table-card']")
+
+print("Total no of rows in a table are: ",len(noOfRows))
+status_count = 0
+Total = len(noOfRows)
+#Specific Elements Status
+for r in range(1,len(noOfRows)+1):
+    status = driver_obj.find_element(By.XPATH,"//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div["+str(r)+"]/div/div[5]").text
+    if status == "Enabled":
+        status_count += 1
+
+print("Total number of users are: ", Total)
+print("Total number of user in Enabled Status are: ",status_count)
+print("Total number os users in Disabled Status are: ", (Total-status_count))
+
+
+#  //*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div["+str(r)+"]/div/div[5]
+# //*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[2]/div/div[5]
+# //div[@class='oxd-table-body']/div[@class='oxd-table-card']//div[@role='row']//div[@role='cell' and @xpath='5']
